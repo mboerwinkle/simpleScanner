95579a52548483562dd7f1a3f606a279e0db685d
mboerwinkle <mboerwinkle@gmail.com>
2016-12-07 14:57:22 +0000

    fixed triangulation with two cameras.

--- 2createPointCloud/main.cpp
+++ 2createPointCloud/main.cpp
@@ -26,6 +26,7 @@ int main(){
 	while(moreData){
 		light lights[LIGHT_COUNT];
 		for(int light_idx = 0; light_idx < LIGHT_COUNT; light_idx++){
+			printf("\n");
 			light* currLight = &(lights[light_idx]);
 			for(int cam_idx = 0; cam_idx < CAMERA_COUNT; cam_idx++){
 				//find view
@@ -33,9 +34,11 @@ int main(){
 					moreData = false;
 					break;
 				}
+				printf("Light at %lf %lf\n", (currLight->view[cam_idx][0]),(currLight->view[cam_idx][1]));
 				//find vec
 				double dirAngle = currLight->view[cam_idx][0]*0.5*FOV;
 				double dirPitch = currLight->view[cam_idx][1]*0.5*VERT_FOV;
+				printf("%lf degrees rotation\n%lf degrees elevation\n", 180*dirAngle/M_PI, 180*dirPitch/M_PI);
 				double tempVec[3] = {-(double)CAMX[cam_idx], -(double)CAMY[cam_idx], 0};
 				if(tempVec[0] == 0) puts("please choose a y placement for the camera that is NOT 0.");//FIXME
 				norm(tempVec);
@@ -48,6 +51,7 @@ int main(){
 				currLight->vec[cam_idx][0] = tempVec[0];
 				currLight->vec[cam_idx][1] = tempVec[1];
 				currLight->vec[cam_idx][2] = tempVec[2];
+				printf("final vector: %lf %lf %lf\n", tempVec[0], tempVec[1], tempVec[2]);
 			}
 			if(!moreData) break;
 			//find loc. This is a REALLY dumb way of doing it.
@@ -64,6 +68,7 @@ int main(){
 			for(int y = loc[1]-1; y <= loc[1]+1; y++){
 			for(int z = loc[2]-1; z <= loc[2]+1; z++){
 				if(findTotalOffset(currLight, bestx, besty, bestz) > findTotalOffset(currLight, x, y, z)){
+//					printf("Better Found: %d %d %d\nBetter Offset: %d\n", x, y, z, findTotalOffset(currLight, x, y, z));
 					bestx = x;
 					besty = y;
 					bestz = z;
@@ -71,7 +76,7 @@ int main(){
 			}}}
 			}
 			//end of dumb code region (Relatively)
-			printf("%d: %ld %ld %ld\n", light_idx, loc[0], loc[1], loc[2]);
+			printf("FINAL LOCATION %d: %ld %ld %ld\nOFFSET %d\n", light_idx, loc[0], loc[1], loc[2], findTotalOffset(currLight, bestx, besty, bestz));
 		}
 	}
 	for(int cam = 0; cam < CAMERA_COUNT; cam++){
@@ -80,19 +85,23 @@ int main(){
 	return 0;
 }
 //finds the distance between vector v(xyz) and point xyz
-int offset(double vx, double vy, double vz, int x, int y, int z){
-	double normalPointVector[3] = {(double)x, (double)y, (double)z};
-	norm(normalPointVector);//the vector that points the same direction as the xyz point.
-/*	double angle = acos(vx*x+vy*y+vz*z);
-	cos(angle)*sqrt(x*x+y*y+z*z);
-*/
-	return (vx*x+vy*y+vz*z)*sqrt(x*x+y*y+z*z);
+double offset(double vx, double vy, double vz, int x, int y, int z){
+	double nPV[3] = {(double)x, (double)y, (double)z};//normal point vector
+	norm(nPV);//the vector that points the same direction as the xyz point.
+	double angle = acos(vx*nPV[0]+vy*nPV[1]+vz*nPV[2]);
+//	printf("ANGLE %lf\n", angle);
+	double ret = fabs(sin(angle)*sqrt(x*x+y*y+z*z));
+//	printf("%lf %lf %lf\nOFFSET: %lf\n", vx, vy, vz, ret);
+	return ret;
 }
 int findTotalOffset(light* targ, int x, int y, int z){
+//	printf("BEGINNING OF OFFSET CALCULATION\n");
+	//the old method just added the offsets. This one adds the squares of the offsets. tends towards smaller equal numbers.
 	int ret = 0;
 	for(int cam_idx = 0; cam_idx < CAMERA_COUNT; cam_idx++){
-		ret += offset(targ->vec[cam_idx][0], targ->vec[cam_idx][1], targ->vec[cam_idx][2], x-CAMX[cam_idx], y-CAMY[cam_idx], z);
+		ret += pow(offset(targ->vec[cam_idx][0], targ->vec[cam_idx][1], targ->vec[cam_idx][2], x-CAMX[cam_idx], y-CAMY[cam_idx], z), 2);
 	}
+//	printf("END OF OFFSET CALCULATION\n");
 	return ret;
 }
 
--- shared/defs.cpp
+++ shared/defs.cpp
@@ -1,5 +1,5 @@
 #include <inttypes.h>
 #include "defs.h"
-uint32_t LIGHT_COLOR[LIGHT_COUNT] = {RED, BLUE};
+uint32_t LIGHT_COLOR[LIGHT_COUNT] = {BLUE, RED};
 int32_t CAMX[CAMERA_COUNT] = {-10000, 10000};
 int32_t CAMY[CAMERA_COUNT] = {-10000, -10000};
